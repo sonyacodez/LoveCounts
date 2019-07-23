976649690e1f0fb48d6c57cab29d55b797a43202
Sonya Burlan <Sonya@Sonyas-MacBook-Air.local>
2019-07-23 15:04:56 +0300

    Created router.get(/transactions)

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,17 +6,14 @@ const UserCouple = require('../models/UserCouple')
 // const APIkey = "f37bef48cce84ed092b150636191707"
 // const moment = require('moment')
 
-// router.get('/transactions', (req,res)=>{
-//     const coupleKey = req.body
-//     UserCouple.findById(`${coupleKey}`)
-//         .populate("transactions")
-//         .exec((err,usercouple)=>{res.send(usercouple.transactions)})
-//         .sort({date: -1})
-// })
-    // GET(‘/transactions): 
-    // receives the key
-    // Searches for the specific user instance with the key.
-    // Retrieves the transactions array of objects
-    // Sends the transactions array to the Model’s getTransactions.
+router.get('/transactions', (req,res)=>{
+    const coupleKey = req.body
+    UserCouple.findById(`${coupleKey.key}`)
+        .populate("transactions")
+        .exec((err,usercouple)=>{
+            res.send(usercouple.transactions)
+        })
+        .sort({date: -1})
+})
 
 module.exports = router
