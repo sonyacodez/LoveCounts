3eb642b41837e863d5bb4e891eede02e4eebf81c
Katya <lopukhina.ekaterina@gmail.com>
2019-07-23 18:43:02 +0300

    commented out other methods

--- dist/LoveManager.js
+++ dist/LoveManager.js
@@ -5,31 +5,31 @@ class LoveManager {
     }
     //sends a transaction data as POST request to the /expenses post route on the server
 
-    addExpense(transaction){
-        const transactionObject = {
-            category: transaction.category,
-            type: transaction.type,
-            amount: transaction.amount, 
-            date: transaction.date, 
-            comment: transaction.comment,
-            coupleKey: transaction.coupleKey
-        }
-        this.allTransactions.push(transactionObject)
-        $.post(`/expenses`,transactionObject,function (res) {
-            console.log("transaction POST complete")
-    })
-}
+//     addExpense(transaction){
+//         const transactionObject = {
+//             category: transaction.category,
+//             type: transaction.type,
+//             amount: transaction.amount, 
+//             date: transaction.date, 
+//             comment: transaction.comment,
+//             coupleKey: transaction.coupleKey
+//         }
+//         this.allTransactions.push(transactionObject)
+//         $.post(`/expenses`,transactionObject,function (res) {
+//             console.log("transaction POST complete")
+//     })
+// }
     //sends a goal data as POST request to the /goal post route on the server
 
-    addFavGoal(goalData){
-        const goalObject = {
-            coupleKey: goalData.coupleKey,
-            goal: goalData.goal
-        }
-        $.post('goal', goalObject, function(res){
-            console.log("goal POST complete")
-        })
-}
+    // addFavGoal(goalData){
+    //     const goalObject = {
+    //         coupleKey: goalData.coupleKey,
+    //         goal: goalData.goal
+    //     }
+    //     $.post('goal', goalObject, function(res){
+    //         console.log("goal POST complete")
+    //     })
+// }
     async getTransactions(coupleKey){
         let transactions = await $.get(`/transactions/${coupleKey}`)
         console.log(transactions)
@@ -37,27 +37,27 @@ class LoveManager {
     }
 
 
-    async getGoals(coupleKey){
-        let result = await $.get(`/goals/${coupleKey}`)
-        return result
-        }
+    // async getGoals(coupleKey){
+    //     let result = await $.get(`/goals/${coupleKey}`)
+    //     return result
+    //     }
     
 
-    async getPartner(userData){
-        let result = await $.get('/partner', {coupleKey: userData.coupleKey, userName: userData.userName})
-        return result.partnerName
-        }
+    // async getPartner(userData){
+    //     let result = await $.get('/partner', {coupleKey: userData.coupleKey, userName: userData.userName})
+    //     return result.partnerName
+    //     }
 
 
-    async checkPassword(userEmail, password){
-        let rightPassword = await $.get(`password/${userEmail}`)
-        if(password === rightPassword){
-            return true;
-        } else {
-            return false;
-        }
-    }
+    // async checkPassword(userEmail, password){
+    //     let rightPassword = await $.get(`password/${userEmail}`)
+    //     if(password === rightPassword){
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
 }
 
-const loveManager = new LoveManager()
-loveManager.getTransactions("5d370810c6046607fc5e5e56")
+// const loveManager = new LoveManager()
+// loveManager.getTransactions("5d370810c6046607fc5e5e56")
