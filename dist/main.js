07afdc3be538d7ba9efe13257f66c53fbe465f1a
Liat <liatcohen9@gmail.com>
2019-07-23 19:15:44 +0300

    load transactions from DB to page

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,6 @@
 const renderer = Render()
-// const managner = LoveManager()
-let coupleKey = '5d370a84b925c61d7b0e0fa9', userName
+const manager = new LoveManager()
+let coupleKey = '5d370f800f89019d266989ec', userName
 
 
 // Sends userKey to getTransactions function(in model)
@@ -9,12 +9,13 @@ let coupleKey = '5d370a84b925c61d7b0e0fa9', userName
 
 const loadTransactionPage = async function () {
     console.log("loadTransactionPage")
-    // await const expenses =manager.getTransactions(coupleKey)
-    const expenses = [
-        { type: 'exspense', category: 'food', amount: '30', date: '13/3/12', comment: 'food is good' },
-        { type: 'income', category: 'salary', amount: '4000000', date: '3/3/2020', comment: 'work work work' },
-        { type: 'exspense', category: 'fun', amount: '100', date: '1/1/1111', comment: 'my comment' }
-    ]
+     await manager.getTransactions(coupleKey)
+     const expenses =manager.allTransactions
+    // const expenses = [
+    //     { type: 'exspense', category: 'food', amount: '30', date: '13/3/12', comment: 'food is good' },
+    //     { type: 'income', category: 'salary', amount: '4000000', date: '3/3/2020', comment: 'work work work' },
+    //     { type: 'exspense', category: 'fun', amount: '100', date: '1/1/1111', comment: 'my comment' }
+    // ]
     expenses.forEach(e => { e.type === 'exspense' ? e.type = true : e.type = false })
     renderer.renderTransactionPage(expenses)
 
