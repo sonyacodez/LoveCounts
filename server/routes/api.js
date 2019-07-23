4878ab53d889b4b36e6abc16162d58d87d4723a8
Sonya Burlan <Sonya@Sonyas-MacBook-Air.local>
2019-07-23 16:35:35 +0300

    Created Add Expense Route at router.post('/expenses')

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,18 +2,37 @@ const express = require('express')
 const router = express.Router()
 const request = require('request')
 const UserCouple = require('../models/UserCouple')
-// const Transaction = require('../models/Transaction')
-// const APIkey = "f37bef48cce84ed092b150636191707"
-// const moment = require('moment')
+const Transaction = require('../models/Transaction')
+const moment = require('moment')
 
 router.get('/transactions', (req,res)=>{
     const coupleKey = req.body
-    UserCouple.findById(`${coupleKey.key}`)
+    UserCouple.findById(`${coupleKey.coupleKey}`)
         .populate("transactions")
+        .sort({date: -1})
         .exec((err,usercouple)=>{
             res.send(usercouple.transactions)
         })
-        .sort({date: -1})
 })
+
+router.post('/expenses', function(req,res){
+    const expenseInfo = req.body
+    const coupleKey = req.body.coupleKey
+    const expenseDate = req.body.date ? moment(req.body.date).format('LLLL') : null
+    const newExpense = new Transaction ({
+        type: expenseInfo.type,
+        category: expenseInfo.category,
+        amount: expenseInfo.amount,
+        date: expenseDate,
+        comment: expenseInfo.comment
+    })
+    newExpense.save()
+    UserCouple.findById(coupleKey, (err,response)=>{
+        response.transactions.push(newExpense)
+        response.save()
+        res.end()
+    })
+})
+
 
 module.exports = router
