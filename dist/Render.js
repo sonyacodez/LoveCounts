b728d3555e0072a88dad072b93f3d102689ed719
Liat <liatcohen9@gmail.com>
2019-07-25 00:56:18 +0300

    new chart

--- dist/Render.js
+++ dist/Render.js
@@ -45,7 +45,6 @@ const Render = function () {
     const renderSportsEvents = function (events) {
         $(".goal-suggestions-div").empty()
         $('#form-container').empty()
-
         $("#loading-gif").empty()
         const source = $('#events-template').html();
         const template = Handlebars.compile(source);
@@ -54,14 +53,14 @@ const Render = function () {
         $('#form-container').append(newHTML);
     }
 
-    const renderLoading= function(){
+    const renderLoading = function () {
         $('#form-container').append(`<div id="loading-gif"><img src="../pics/tenor.gif"></div>`)
     }
 
     const renderRecTravelForm = function (favGoals, savings) {
         $('#form-container').empty()
 
-            $('#form-container').append(
+        $('#form-container').append(
             `<div id="flight-search-form">
                 <div class="travel-form-item row">
                     <div class="input ">
@@ -88,13 +87,75 @@ const Render = function () {
     const renderReportPage = function (categories, amount, savings) {
         $("#container").empty()
         $('#container').append(`<canvas id="myChart"></canvas>`);
-        renderChart(categories, amount)
+        renderBarChart(categories, amount)
+        // renderPieChart(categories, amount)
         $('#container').append(`<div class="box3 sb14">You saved <span id="savings"> ${savings}$ </span> this month!<br>
                                     check Recommendations page to see what you can spent them on
                                 </div>`)
     }
 
-    function renderChart(labels, data) {
+    function renderBarChart(labels, data) {
+        // var ctx = document.getElementById("myChart").getContext('2d');
+        var ctx = document.getElementById("myChart");
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labels,//["2015-01", "2015-02", "2015-03", "2015-04", "2015-05", "2015-06", "2015-07", "2015-08", "2015-09", "2015-10", "2015-11", "2015-12"],
+                datasets: [{
+                    label: '# of Tomatoes',
+                    data: data,//[12, 19, 3, 5, 2, 3, 20, 3, 5, 6, 2, 1],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: false,
+                scales: {
+                    xAxes: [{
+                        ticks: {
+                            maxRotation: 90,
+                            minRotation: 80
+                        }
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+
+    }
+
+    function renderPieChart(labels, data) {
         var ctx = document.getElementById("myChart").getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'pie',
